b160fd47f00f28a876c0e61ea7695355497140d4
LacErnest <laceinstein@gmail.com>
2022-09-02 13:50:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ const app = express()
 app.use(express.json())
 
 
-// Use in production to serve client files 
-//let a = 10; b = '10' console.log(a == b) // true 
-// let a = 10; b = 10; c = -10 console.log(a==b)//true, console.log(a==c)//false; console(b!=c)//true; console.log(a != b)//false
-//let str1 = 'Javscript'; let str2 = 'Javascript'; let str3 = 'JavaScript' console.log(str1 == str2)//true; str1 == str3 //false
-//let a = 10; let b == '10'; let c= 10; console.log(a===b)//false; console.log(a === c )//true
-// 34 === '34' //false 
-// === returns true only if operands are of the same data type and same value, otherwise returns false
-
 if(process.env.NODE_ENV  === 'production'){
   app.use(express.static('client/build'));
   app.get('*', (req, res) =>{
